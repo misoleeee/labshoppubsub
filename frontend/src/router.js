bb1d8daa50abec31c79dde28430fc0f3ddeba8a5
misoleeee <156728271+misoleeee@users.noreply.github.com>
2024-01-25 14:29:50 +0900

    commit

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,29 +5,18 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 
-import OrderOrderManager from "./components/listers/OrderOrderCards"
-import OrderOrderDetail from "./components/listers/OrderOrderDetail"
-
 import InventoryInventoryManager from "./components/listers/InventoryInventoryCards"
 import InventoryInventoryDetail from "./components/listers/InventoryInventoryDetail"
 
+
+import Manager from "./components/listers/Cards"
+import Detail from "./components/listers/Detail"
 
 
 export default new Router({
     // mode: 'history',
     base: process.env.BASE_URL,
     routes: [
-            {
-                path: '/orders/orders',
-                name: 'OrderOrderManager',
-                component: OrderOrderManager
-            },
-            {
-                path: '/orders/orders/:id',
-                name: 'OrderOrderDetail',
-                component: OrderOrderDetail
-            },
-
             {
                 path: '/inventories/inventories',
                 name: 'InventoryInventoryManager',
@@ -39,6 +28,17 @@ export default new Router({
                 component: InventoryInventoryDetail
             },
 
+
+            {
+                path: '//',
+                name: 'Manager',
+                component: Manager
+            },
+            {
+                path: '///:id',
+                name: 'Detail',
+                component: Detail
+            },
 
 
 
